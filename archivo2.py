d8d10723ede5566556b060b17e825ec7087dc2bc
Manuel Albo <manuelappleid04@gmail.com>
2023-09-01 11:54:43 -0400

    Agregando funcion

--- archivo2.py
+++ archivo2.py
@@ -1,4 +1,8 @@
+
 print("Chanchito felih")
+
+def pinga():
+    pass
 print("Como te ha ido estas soltera o tienes marcido, se que es informal perdone lo atrevido")
 print("Csmbios een stage")
 class Persona:
